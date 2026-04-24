d83f995342869f0e3cf4f88e88974b1730b1ba97
Sanjay Choudhary <sanjaytech6375@gmail.com>
2026-04-24 18:57:27 +0530

    add portfolio page

--- src/app/(root)/portfolio/page.tsx
+++ src/app/(root)/portfolio/page.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { StatsSection } from "@/components/shared/StatsSection";
+import { HeroSection } from "./components/HeroSection";
+import { CTASection } from "@/components/shared/CTASection";
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <main className="min-h-screen text-slate-900 dark:text-slate-100">
+      <HeroSection />
+      <StatsSection />
+      <CTASection
+        title={
+          <>
+            Your Project Could Be Next
+          </>
+        }
+        description="Let's build something extraordinary together. Share your idea — we respond within 24 hours."
+        primaryBtnText="Start a Project"
+        secondaryBtnText="Explore the services →"
+      />
+    </main>
+  );
+};
 
-export default page
+export default page;
